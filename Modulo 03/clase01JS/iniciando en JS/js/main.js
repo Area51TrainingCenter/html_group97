aa315b43aaf4a6665113928529bfb33a1613accb
jtorriccelli <jcarlos@sminnova.com>
2017-12-01 17:27:12 -0500

    update

---
+++ Modulo 03/clase01JS/iniciando en JS/js/main.js
@@ -0,0 +1,48 @@
+/*console.log("Bienvenido al mundo de JS");
+console.log("hola")
+console.log("bienvenido")
+console.log("nuevo contenido");*/
+
+// variable 
+
+var dato1;
+var dato2;
+var dato3;
+var dato4;
+
+
+
+dato1="y el dijo :'hola' ";
+dato2="999999999";
+dato3=324;
+
+var num1;
+var num2;
+var num3;
+var resl1;
+var resl2;
+num1=10;
+num2=15;
+num3=30;
+resl1=num1+num2+num3;
+resl2=resl1/2;
+console.log("la suma de 3 num divida entre 2 es igual a: ")
+console.log(resl2);
+
+var nombre="juan carlos";
+var apellidoP="Ramos"
+var apellidoM="Torriccelli";
+
+var nombrecompleto=nombre+" "+apellidoP+" "+apellidoM;
+/* 
+juan carlos Ramos Torriccelli
+*/
+
+var _num1="10";
+var _num2="16";
+var rsl;
+var nuevo_n1=parseInt(_num1);
+var nuevo_n2=parseInt(_num2)
+rsl=nuevo_n1+nuevo_n2;
+
+"1020"
---
+++ Modulo 03/clase01JS/prompt/js/main.js
@@ -0,0 +1,19 @@
+//ventana de alerta
+//alert("Welcome!!");
+//prompt()
+/*var n1=prompt("Ingresa el 1er numero");
+var n2=prompt("Ingresa el 2do numero");
+
+var num1=parseInt(n1);
+var num2=parseInt(n2);
+console.log(num1+num2);
+*/
+var num_mayor=Math.max(10,22,34,9997);
+var num_menor=Math.min(-0.5,22,34,9997);
+console.log(num_mayor);
+console.log(num_menor);
+
+Math.pow(2,5);
+Math.sqrt(169);
+var aleatorio=Math.random()*1000;
+console.log(Math.round(aleatorio));
